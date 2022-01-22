c28d59060822a6601df6b69c78d9a75e9e0ca3fe
Lars S <lars.silberg@itc-studenten.de>
2022-01-22 13:53:01 +0100

    änderungen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import myVideo from './videos/testvideo.mp4'
+import {VideoScroll} from 'react-video-scroll'
+
+const setStyles = (wrapperEl, videoEl, playbackRate) => {
+  wrapperEl.style.marginTop = `calc(180% - ${Math.floor(videoEl.duration) *
+    playbackRate +
+    'px'})`
+  wrapperEl.style.marginBottom = `calc(180% - ${Math.floor(videoEl.duration) *
+    playbackRate +
+    'px'})`
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VideoScroll
+      onLoad={props =>
+        setStyles(props.wrapperEl, props.videoEl, props.playbackRate)
+      }
+      playbackRate={30}
+      style={{ position: 'sticky' }}
+    >
+        <video src={myVideo}tabIndex="0"
+        autobuffer="autobuffer"
+        preload="preload"
+        style={{ width: '100%', objectFit: 'contain' }}
+        playsInline>
+        <source type="video/mp4" src="./oculus.mp4" />
+      </video>
+    </VideoScroll>
   );
 }
 
